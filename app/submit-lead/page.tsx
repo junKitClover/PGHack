ea44bd4dec2dabbf61643abf55b2c6bc62f141f1
“Jun <jun.kit@iproperty.com.my>
2022-12-05 18:42:40 +0700

    update registration page

--- app/submit-lead/page.tsx
+++ app/submit-lead/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Grid, Stack, Flex } from 'component/organisms';
-import { Box, Text, Visible } from 'component/atoms';
+import { Grid, Stack, Flex, PageLayout } from 'component/organisms';
+import { Text, Visible, Box } from 'component/atoms';
 import { Button } from 'component/molecules';
-import { FormEvent, useEffect, useState } from 'react';
-import Router, { useRouter } from 'next/router';
+import { PROJECT_LEAD_INFO } from 'state/projectState';
+import { FormEvent, useState } from 'react';
+import { useAtom } from 'jotai';
 import Link from 'next/link';
+import styles from './page.module.scss';
 
 interface FirstFormProps {
   onSubmit: (event: FormEvent<HTMLFormElement>) => void;
@@ -13,55 +15,35 @@ interface FirstFormProps {
 
 const FirstForm = ({ onSubmit }: FirstFormProps) => {
   return (
+    <Box backgroundColor="white" border rounded marginTop={10}>
     <Stack gap={5} paddingBlock={[2, 3, 6]} paddingInline={[2, 3, 5]}>
       <Flex justifyContent="center" alignItem="center" gap={10} direction="column">
         <Text type="title">Leads Submission Form</Text>
         <form onSubmit={onSubmit} method="post">
           <Grid col={[1, 2]} gap={[2, 3, 4]} padding={[2, 3, 5]}>
             <Text as="label">Name:</Text>
-            <input name="name" type="text" placeholder="Name" />
+            <input name="name" type="text" placeholder="Name" className={styles.inputField} />
             <Text as="label">Phone Number:</Text>
-            <input name="phoneNumber" type="text" placeholder="Phone Number" />
+            <input name="phoneNumber" type="text" placeholder="Phone Number" className={styles.inputField} />
             <Text as="label">Email:</Text>
-            <input name="email" type="text" placeholder="Email" />
+            <input name="email" type="text" placeholder="Email" className={styles.inputField} />
+            <Text as="label">Interested Project:</Text>
+            <input name="interestedProject" type="text" placeholder="Email" className={styles.inputField} />
             <Text as="label">Budget:</Text>
-            <input name="email" type="text" placeholder="Budget" />
+            <input name="budget" type="text" placeholder="Budget" className={styles.inputField} />
             <Text as="label">Looking for:</Text>
-            <input name="email" type="text" placeholder="Looking for" />
+            <input name="lookingfor" type="text" placeholder="Looking for" className={styles.inputField} />
           </Grid>
           <Flex justifyContent="spaceBetween" alignItem="center" marginBlock={4}>
-            <Button type='text'>Clear</Button>
-            <Button>Next</Button>
+            <button type='reset' className={styles.resetBtn}>Clear</button>
+            <button type='submit' className={styles.submitBtn}>Submit</button>
           </Flex>
         </form>
       </Flex>
     </Stack>
+    </Box>
   )
 };
-
-const SecondForm = ({ onSubmit }: FirstFormProps) => {
-  return (
-    <Stack gap={5} paddingBlock={[2, 3, 6]} paddingInline={[2, 3, 5]}>
-      <Flex justifyContent="center" alignItem="center" gap={10} direction="column">
-        <Text type="title">OTP Verification</Text>
-        <form onSubmit={onSubmit} method="post">
-          <Grid col={4} gap={10} padding={[2, 3, 5]}>
-            <input name="1" type="number" placeholder="" />
-            <input name="2" type="number" placeholder="" />
-            <input name="3" type="number" placeholder="" />
-            <input name="4" type="number" placeholder="" />
-          </Grid>
-          <Flex justifyContent="spaceBetween" alignItem="center" marginBlock={4}>
-            <Button type='text'>Clear</Button>
-            <Button>Next</Button>
-          </Flex>
-        </form>
-      </Flex>
-    </Stack>
-  )
-};
-
-
 const Done = () => {
   return (
     <Stack gap={5} paddingBlock={[2, 3, 6]} paddingInline={[2, 3, 5]}>
@@ -75,42 +57,30 @@ const Done = () => {
   )
 };
 
-type TStatus = 'INIT' | 'OTP' | 'FINAL';
-type TStep = 1 | 2 | 3;
+type TStatus = 'INIT' | 'FINAL';
 
 function Page() {
   const [status, setStatus] = useState<TStatus>('INIT');
-  const [step, setStep] = useState<TStep>(1);
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => { 
-    event.preventDefault(); 
-    setStatus('OTP'); 
-    setStep(2); 
-    return false; 
-  };
-  const onSubmitOTP = (event: FormEvent<HTMLFormElement>) => { 
-    event.preventDefault(); 
-    setStatus('FINAL'); 
-    setStep(3);
-    return false; 
+  const [leadRegister, setLeadRegister] = useAtom(PROJECT_LEAD_INFO);
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const [{ value: name }, , { value: email }, { value: projectName }] = event.target as unknown as any;
+    setLeadRegister([...leadRegister, { name, email, projectName }])
+    setStatus('FINAL');
+    return false;
   };
 
   return (
-    <Stack gap={10}>
-      <Flex paddingBlock={5} backgroundColor="white" justifyContent="center" alignItem="center" gap={8} >
-        <Button type="contained" >1</Button>
-        <Button type={step >= 2 ? "contained" : "outline"}  disabled={step < 2}>2</Button>
-        <Button type={step >= 3 ? "contained" : "outline"}  disabled={step < 3}>3</Button>
-      </Flex>
-      <Visible visible={status === 'INIT'}>
-        <FirstForm onSubmit={onSubmit}/>
-      </Visible>
-      <Visible visible={status === 'OTP'}>
-        <SecondForm onSubmit={onSubmitOTP}/>
-      </Visible>
-      <Visible visible={status === 'FINAL'}>
-        <Done />
-      </Visible>
-    </Stack>
+    <PageLayout>
+      <Stack gap={10}>
+        <Visible visible={status === 'INIT'}>
+          <FirstForm onSubmit={onSubmit} />
+        </Visible>
+        <Visible visible={status === 'FINAL'}>
+          <Done />
+        </Visible>
+      </Stack>
+    </PageLayout>
   );
 }
 
